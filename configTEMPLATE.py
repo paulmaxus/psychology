373106ab641ce004fde0838c3b5c3ae9e5c8ae3f
Max Paulus <m.b.paulus@vu.nl>
2022-07-12 15:34:39 +0200

    network visualisation

---
+++ configTEMPLATE.py
@@ -0,0 +1,3 @@
+project_path = ''  # teams/sharepoint, .../Proces Bibliometrics - Data_RI/Psychologie
+elsevier_apikey = ''
+elsevier_instkey = ''  # needed if working from home (outside of VU IP range)
