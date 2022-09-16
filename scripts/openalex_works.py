02a50690deed85a2db2e811ce63fbbd7f6b552ce
Max Paulus <m.b.paulus@vu.nl>
2022-09-16 13:59:15 +0200

    concepts network images

--- scripts/openalex_works.py
+++ scripts/openalex_works.py
@@ -26,17 +26,18 @@ def get_works(works_filter, cursor='*'):
 
 
 def main():
-    institutes = pd.read_csv('../institutes.csv')
+    institutes = pd.read_csv('../affiliations.csv')
     year_range = range(2020, 2023)
     publications = []
     for index, row in institutes.iterrows():
         for year in year_range:
-            works_filter = f'authorships.institutions.ror:{row["ROR"]},publication_year:{year}'
+            works_filter = f'authorships.institutions.ror:{row["ror"]},publication_year:{year}'
             pubs = get_works(works_filter)
             for pub in pubs:
-                pub['ror'] = row['ROR']
+                pub['ror'] = row['ror']
                 pub['year'] = year
             publications.extend(pubs)
     # store as json
     with open('../data/works.json', 'w') as f:
         f.write(json.dumps(publications))
+
